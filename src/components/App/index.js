57eb8f0b2988f57ac374e9b56b2acd7d0bf15c39
Paolo Duzioni <paolo.duzioni@gmail.com>
2020-08-20 14:34:25 +0200

    update title icon

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ import Container from '../layout/Container';
 import MainContent from '../layout/MainContent';
 import Title from '../global/Title';
 import Icon from '../global/Icon';
-import logo from '../../assets/react-icon.svg';
+import logo from '../../assets/app-icon.svg';
 import UploadImage from './UploadImage';
 import TextImage from './TextImage';
 import GenerateImage from './GenerateImage';
@@ -15,7 +15,7 @@ const App = () => {
         <AppWrapper>
             <Container>
                 <Title primary fsize="2" margin="0 0 2rem">
-                    <Icon src={logo} width="50" alt="meme generator" />
+                    <Icon src={logo} alt="The MEME Generator" />
                     The MEME Generator
                 </Title>
 
--- src/components/global/Icon.js
+++ src/components/global/Icon.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 const Icon = styled.img`
-    width: 50px;
-    vertical-align: middle;
     transform: translateY(-3px);
+    width: 40px;
+    margin-right: 0.75rem;
+    vertical-align: middle;
+    border-radius: 4px;
 `;
 
 export default Icon;
